b3e6a51c87140922419cf1fbe4de4ab6e79dd2cd
denis909 <denis909@mail.ru>
2020-07-25 15:12:03 +0300

    n/a

--- JoinWithRelationTrait.php
+++ JoinWithRelationTrait.php
@@ -7,14 +7,19 @@ trait JoinWithRelationTrait
 
 	protected $_joinWithRelation = [];
 
-	public function joinWithRelation(string $name)
+	public function joinWithRelation(string $name, $key = null)
 	{
-		if (array_search($key, $this->_joinWithRelation) !== false)
+		if (array_search($key ?? $name, $this->_joinWithRelation) !== false)
 		{
 			return $this;
 		}
 
-		$this->_joinWithRelation[] = $key;
+		$this->_joinWithRelation[] = $key ?? $name;
+
+		if ($key)
+		{
+			$name .= ' ' . $key;
+		}
 
 		return $this->joinWith($name);
 	}
